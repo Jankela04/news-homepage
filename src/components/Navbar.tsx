894c82a9394f3e3e700cbff3e0964a8a84a9b015
Jankela04 <janezjovanovic04@gmail.com>
2023-04-13 13:47:48 +0200

    create navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,88 @@
+import logo from "/logo.svg";
+import openMenu from "/icon-menu.svg";
+import closeMenu from "/icon-menu-close.svg";
+
+import { useEffect, useState } from "react";
+
+export const Navbar = () => {
+    const [isNavOpen, setIsNavOpen] = useState(false);
+
+    const toggleNavbar = () => {
+        setIsNavOpen((prev) => !prev);
+    };
+
+    useEffect(() => {
+        window.addEventListener(
+            "resize",
+            () => window.innerWidth >= 768 && setIsNavOpen(false)
+        );
+
+        return () => {
+            window.removeEventListener(
+                "resize",
+                () => window.innerWidth >= 768 && setIsNavOpen(false)
+            );
+        };
+    }, []);
+
+    return (
+        <nav className="relative flex justify-between py-10">
+            <a href="/">
+                <img src={logo} alt="Logo" />
+            </a>
+            <button className="z-10 md:hidden" onClick={toggleNavbar}>
+                {isNavOpen ? (
+                    <img src={closeMenu} alt="Close Menu" />
+                ) : (
+                    <img src={openMenu} alt="Open Menu" />
+                )}
+            </button>
+            <div
+                className={
+                    isNavOpen
+                        ? "absolute right-0 top-0 flex h-screen w-2/3 flex-col gap-5 pt-48 md:flex"
+                        : "hidden gap-8 md:flex"
+                }
+            >
+                <Links />
+            </div>
+        </nav>
+    );
+};
+
+const Links = () => {
+    return (
+        <>
+            <a
+                className="ml-4 text-lg font-semibold text-black hover:text-primary-orange md:ml-0 md:font-normal md:text-neutral-darkGrayBlue"
+                href="/"
+            >
+                Home
+            </a>
+            <a
+                className="ml-4 text-lg  font-semibold text-black hover:text-primary-orange md:ml-0 md:font-normal md:text-neutral-darkGrayBlue"
+                href="/"
+            >
+                New
+            </a>
+            <a
+                className="ml-4 text-lg  font-semibold text-black hover:text-primary-orange md:ml-0 md:font-normal md:text-neutral-darkGrayBlue"
+                href="/"
+            >
+                Popular
+            </a>
+            <a
+                className="ml-4 text-lg  font-semibold text-black hover:text-primary-orange md:ml-0 md:font-normal md:text-neutral-darkGrayBlue"
+                href="/"
+            >
+                Trending
+            </a>
+            <a
+                className="ml-4 text-lg  font-semibold text-black hover:text-primary-orange md:ml-0 md:font-normal md:text-neutral-darkGrayBlue"
+                href="/"
+            >
+                Categories
+            </a>
+        </>
+    );
+};
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,17 +2,18 @@
 module.exports = {
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
     theme: {
-        extend: {},
-        colors: {
-            primary: {
-                orange: "hsl(35, 77%, 62%)",
-                red: "hsl(5, 85%, 63%)",
-            },
-            neutral: {
-                offWhite: "hsl(36, 100%, 99%)",
-                grayBlue: "hsl(233, 8%, 79%)",
-                darkGrayBlue: "hsl(236, 13%, 42%)",
-                darkBlue: "hsl(240, 100%, 5%)",
+        extend: {
+            colors: {
+                primary: {
+                    orange: "hsl(35, 77%, 62%)",
+                    red: "hsl(5, 85%, 63%)",
+                },
+                neutral: {
+                    offWhite: "hsl(36, 100%, 99%)",
+                    grayBlue: "hsl(233, 8%, 79%)",
+                    darkGrayBlue: "hsl(236, 13%, 42%)",
+                    darkBlue: "hsl(240, 100%, 5%)",
+                },
             },
         },
     },
