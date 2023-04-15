01411b6fb8e021cad1e3f08ffecbb90439f6a432
Jankela04 <janezjovanovic04@gmail.com>
2023-04-15 22:36:07 +0200

    finish News Component

---
+++ src/components/News.tsx
@@ -0,0 +1,40 @@
+const News = () => {
+    return (
+        <div className="bg-neutral-darkBlue p-6">
+            <h2 className="pb-10 text-5xl font-bold text-primary-orange">
+                New
+            </h2>
+            <NewSection
+                title="Hydrogen VS Electric Cars"
+                body="Will hydrogen-fueled cars ever catch up to EVs"
+            />
+            <LineBreak />
+            <NewSection
+                title="Hydrogen VS Electric Cars"
+                body="Will hydrogen-fueled cars ever catch up to EVs"
+            />
+            <LineBreak />
+            <NewSection
+                title="Hydrogen VS Electric Cars"
+                body="Will hydrogen-fueled cars ever catch up to EVs"
+            />
+        </div>
+    );
+};
+
+export default News;
+
+const NewSection = ({ title, body }: { title: string; body: string }) => {
+    return (
+        <div className="text-white">
+            <a className="mb-3 block cursor-pointer text-2xl font-bold hover:text-primary-orange">
+                {title}
+            </a>
+            <p className="text-lg text-neutral-grayBlue">{body}</p>
+        </div>
+    );
+};
+
+const LineBreak = () => {
+    return <div className="my-8 h-[1px] w-full bg-neutral-darkGrayBlue"></div>;
+};
